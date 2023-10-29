236721e2b81c1626d1db93e3b9ea44521f2f846f
Roman Makhnev <romanmahnef777@gmail.com>
2023-10-29 15:52:23 +0400

    Upper and title

--- str_func.py
+++ str_func.py
@@ -1,5 +1,10 @@
 def tit():
+    '''Transform symbols '''
     first_word = input().upper()
     return first_word
 
-print(tit())
+
+def tit1():
+    '''Title input'''
+    second_word = input().title()
+    return second_word
